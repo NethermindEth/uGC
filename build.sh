628238715bcb36fede4dfed32ecb6be8febd6c8e
Maxim Menshikov <maxim@menshikov.org>
2025-10-19 14:51:01 +0100

    Add rv64ima toolchain

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export TOPDIR="$(cd "$(dirname "$(which "$0")")" ; pwd -P)"
+
 if [ "${RUNTIME_BASE_DIR}" == "" ] ; then
     echo "RUNTIME_BASE_DIR is not set" >&2
     exit 1
@@ -10,6 +12,7 @@ pushd build > /dev/null 2> /dev/null
     cmake -GNinja \
           -DUGC_RUNTIME_INCLUDE_DIR=${RUNTIME_BASE_DIR} \
           -DCMAKE_INSTALL_PREFIX=$(pwd)/usr \
+          -DCMAKE_TOOLCHAIN_FILE=${TOPDIR}/toolchain/rv64ima.cmake \
           ..
     ninja
     ninja install
