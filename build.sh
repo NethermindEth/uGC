551b72897fc4a846dd2d6fd867c85f4a5e3382e0
Maxim Menshikov <maxim@menshikov.org>
2026-01-31 00:02:15 +0000

    build.sh: non-interactive docker

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ is_docker="$1"
 if [ "$is_docker" == "docker" ] ; then
     docker build .
     sha=$(docker build -q .)
-    docker run -v $(pwd):$(pwd) -w $(pwd) -it $sha ./build.sh
+    docker run -v $(pwd):$(pwd) -w $(pwd) $sha ./build.sh
     exit $?
 fi
 
